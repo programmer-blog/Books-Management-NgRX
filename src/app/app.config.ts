f81d0bf8fdf41be3896f2a071d5e06010fb2e8f4
Programmer Blog <programmerblog.net@gmail.com>
2024-07-15 00:10:15 +0300

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { BookEffects } from './books/book.effect'; 
 
 import { routes } from './app.routes';
+import { BookReducer } from './books/book.reducer';
+import { AppState } from './app.state';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideStore<AppState>({ book: BookReducer }),
+    provideEffects([BookEffects]),
+  ]
 };
